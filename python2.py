2de19722a8145f812d15e7fc1b4a9a73be9c6d8d
zhaihotmail <zhaizhongping@hotmail.com>
2025-01-14 21:24:50 -0800

    Delete python2.py

--- python2.py
+++
@@ -1,9 +0,0 @@
-
-import subprocess
-
-def execute_command(command):
-    # This is vulnerable to shell injection attacks
-    subprocess.run(command, shell=True)
-
-# Example usage
-execute_command("ls -la")
